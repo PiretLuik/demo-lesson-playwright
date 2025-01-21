343e66890f80e8efe5cfbaa8d37f68b92705b1a2
PiretLuik <luik.piret@gmail.com>
2025-01-21 23:16:56 +0200

    Pw_17 & 18

--- tests/pages/order-page.ts
+++ tests/pages/order-page.ts
@@ -1,12 +1,31 @@
 import { Locator, Page } from '@playwright/test'
+import { BasePage } from './base-page'
 
-export class OrderPage {
-  readonly page: Page
+export class OrderPage extends BasePage {
+  //private page: Page
   readonly statusButton: Locator
+  readonly orderCreatorName: Locator
+  readonly orderCreatorPhone: Locator
+  readonly orderComment: Locator
+  readonly createOrderButton: Locator
+  readonly logoutButton: Locator
+  readonly orderCreatorPopup: Locator
+  readonly orderSearchPopupFiled: Locator
+  //readonly orderPopupTrackingButton : Locator
+  //readonly orderPopupCloseButton : Locator
   // add more locators here
 
   constructor(page: Page) {
-    this.page = page
-    this.statusButton = page.getByTestId('openStatusPopup-button')
+    super(page)
+    //this.page = page
+    this.statusButton = this.page.getByTestId('openStatusPopup-button')
+    this.orderCreatorName = this.page.getByTestId('username-input')
+    this.orderCreatorPhone = this.page.getByTestId('phone-input')
+    this.orderComment = this.page.getByTestId('comment-input')
+    this.createOrderButton = this.page.getByTestId('createOrder-button')
+    this.logoutButton = this.page.getByTestId('logout-button')
+    this.orderCreatorPopup = this.page.getByTestId('orderSuccessfullyCreated-popup')
+    this.orderSearchPopupFiled = this.page.getByTestId('searchOrder-input')
+    //this.page
   }
 }
--- tests/ui/e2e-flow-with-before.spec.ts
+++ tests/ui/e2e-flow-with-before.spec.ts
@@ -1,7 +1,6 @@
 import { test, expect } from '@playwright/test'
 import { LoginPage } from '../pages/login-page'
 import { faker } from '@faker-js/faker/locale/ar'
-import { PASSWORD, USERNAME } from '../../config/env-data'
 
 let authPage: LoginPage
 
@@ -13,19 +12,30 @@ test.beforeEach(async ({ page }) => {
 test('signIn button disabled when incorrect data inserted', async ({}) => {
   await authPage.usernameField.fill(faker.lorem.word(2))
   await authPage.passwordField.fill(faker.lorem.word(7))
-  await expect(authPage.signInButton).toBeDisabled()
+  await expect.soft(authPage.signInButton).toBeDisabled()
 })
 
 test('error message displayed when incorrect credentials used', async ({}) => {
-  // implement test
+  await authPage.signIn('test', 'test1234')
+  await expect.soft(authPage.incorrectCredentialsPopup).toBeVisible()
+  await expect.soft(authPage.incorrectCredentialsPopup).toContainText('Incorrect credentials')
 })
 
 test('login with correct credentials and verify order creation page', async ({}) => {
-  const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
-  await expect(orderCreationPage.statusButton).toBeVisible()
-  // verify at least few elements on the order creation page
+  const orderCreationPage = await authPage.signIn('piret_luik', 'T5u6V7w8')
+  await expect.soft(orderCreationPage.statusButton).toBeVisible()
+  await expect.soft(orderCreationPage.orderCreatorName).toBeVisible()
+  await expect.soft(orderCreationPage.orderCreatorPhone).toBeVisible()
+  await expect.soft(orderCreationPage.orderComment).toBeVisible()
+  await expect.soft(orderCreationPage.createOrderButton).toBeVisible()
 })
 
 test('login and create order', async ({}) => {
-  // implement test
+  const orderCreationPage = await authPage.signIn('piret_luik', 'T5u6V7w8')
+  await expect.soft(orderCreationPage.orderCreatorName).toBeVisible()
+  await orderCreationPage.orderCreatorName.fill(faker.lorem.word(4))
+  await orderCreationPage.orderCreatorPhone.fill(faker.lorem.lines(6))
+  await orderCreationPage.orderComment.fill(faker.lorem.words(6))
+  await orderCreationPage.createOrderButton.click()
+  await orderCreationPage.orderCreatorPopup.isVisible()
 })
